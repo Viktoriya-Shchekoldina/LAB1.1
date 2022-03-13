1b9fef8d7428adc0c9a4e860a58effe1a92ef5c6
Вика Щеколдина <schekoldina28@mail.ru>
2022-03-13 18:46:20 +0400

    Добавьте файлы проекта.

---
+++ LAB1.1/Registration.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="LAB1._1.Registration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label Text="Login" runat="server" ></asp:Label><br />
+            <asp:TextBox ID="Login" runat="server"></asp:TextBox>
+            <br /><br />
+            <asp:Label Text="Password" runat="server" ></asp:Label><br />
+            <asp:TextBox ID="Password" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Field" runat="server" ></asp:Label>
+            <br /><br />
+            <asp:Button ID="Submit" runat="server" Text="To come in" OnClick="Submit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ LAB1.1/Registration.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LAB1._1
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+
+        public static Dictionary<string, string> Users = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            {"Vika","VVV" },
+            {"KKK" , "y"}
+        };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            if (Users.ContainsKey(Login.Text))
+            {
+                if (Users[Login.Text] == Password.Text)
+                {
+                    Response.Redirect("Welcome.aspx");
+                }
+                else
+                {
+                    Field.Text = "Invalid password entered";
+                }
+            }
+            else
+            {
+                Field.Text = "Invalid login entered";
+            }
+        }
+
+
+    }
+}
